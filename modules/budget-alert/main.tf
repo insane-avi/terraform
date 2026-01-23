878f609d85722556b4a5380f6ecc82520d57420d
insane-avi <aviraldabral@Avirals-MacBook-Air.local>
2026-01-23 15:50:28 +0530

    iu

--- modules/budget-alert/main.tf
+++ modules/budget-alert/main.tf
@@ -1,19 +1,11 @@
-resource "aws_sns_topic" "budget_alerts" {
-  name = "budget-alert-topic"
-}
-
-resource "aws_sns_topic_subscription" "email" {
-  topic_arn = aws_sns_topic.budget_alerts.arn
-  protocol  = "email"
-  endpoint  = var.email_address
-}
-
 resource "aws_budgets_budget" "monthly_budget" {
   name         = var.budget_name
   budget_type = "COST"
+
   limit_amount = var.limit_amount
   limit_unit   = "USD"
-  time_unit    = var.time_unit
+
+  time_unit = var.time_unit
 
   notification {
     comparison_operator = "GREATER_THAN"
@@ -21,9 +13,11 @@ resource "aws_budgets_budget" "monthly_budget" {
     threshold_type      = "PERCENTAGE"
     notification_type   = "ACTUAL"
 
-    subscriber {
-      subscription_type = "SNS"
-      address           = aws_sns_topic.budget_alerts.arn
+    notification_state = "ALARM"
+
+    notification_target {
+      address = aws_sns_topic.budget_alerts.arn
+      type    = "SNS"
     }
   }
 }
